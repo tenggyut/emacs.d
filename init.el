284cec1adf3e1c11811179743425822da7df0f18
tengyt <tengyutong0213@gmail.com>
2015-08-04 17:25:36 +0800

    add evil

--- init.el
+++ init.el
@@ -145,7 +145,15 @@
           (lambda ()
             (message "init completed in %.2fms"
                      (sanityinc/time-subtract-millis after-init-time before-init-time))))
+(require 'evil)
+(evil-mode 1)
 
+(eval-after-load "evil"
+  '(progn
+     (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+     (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+     (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+     (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)))
 
 (provide 'init)
 
